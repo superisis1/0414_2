77b141bca3f9f9ddc23f89db47d81d5f70c0b5d0
Insung Jung <superisis@gmail.com>
2019-04-14 15:31:09 +0900

    XML 아이디 추가 변수명 정리

--- app/src/main/java/kr/insungjung/a0414_2/LoginActivity.java
+++ app/src/main/java/kr/insungjung/a0414_2/LoginActivity.java
@@ -1,5 +1,6 @@
 package kr.insungjung.a0414_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,14 +10,14 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-/*
-ID: tjeit, PW: qwer1234 입력된 상태로
+    /*
+    ID: tjeit, PW: qwer1234 입력된 상태로
     로그인버튼 누르면 베인 액티비티로 이동
     이이디 비번 위의 값이 아니라면, 토스트로 '로그인에 실패하셨습니다. ID/PW
     */
 
-    EditText userIdText;
-    EditText userPasswordText;
+    EditText idText;
+    EditText passText;
     Button loginBtn;
 
     @Override
@@ -24,22 +25,24 @@ ID: tjeit, PW: qwer1234 입력된 상태로
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userIdText = findViewById(R.id.idText);
-        userPasswordText = findViewById(R.id.passText);
+        idText = findViewById(R.id.idText);
+        passText = findViewById(R.id.passText);
         loginBtn = findViewById(R.id.loginBtn);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String userId = userIdText.getText().toString();
-                String userPw = userPasswordText.getText().toString();
+                String userId = idText.getText().toString();
+                String userPw = passText.getText().toString();
 
 //                맞으면 메인화면으로 이동
 //                아니면 토스트 메세지
 
                 if (userId.equals("tjeit") && userPw.equals("qwer1234")) {
 //                  메인 액티비티로 이동
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
 
                 } else {
 //                    조건에 맞지 않다면
