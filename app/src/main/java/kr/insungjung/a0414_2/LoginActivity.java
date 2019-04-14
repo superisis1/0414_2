af88969f3f57b89c866ae4b55f8715334e2874a5
Insung Jung <superisis@gmail.com>
2019-04-14 15:17:04 +0900

    로그인 버튼 이벤트 처리 로그인 실패시 토스트 띄우는 것 까지만 구현

--- app/src/main/java/kr/insungjung/a0414_2/LoginActivity.java
+++ app/src/main/java/kr/insungjung/a0414_2/LoginActivity.java
@@ -2,12 +2,52 @@ package kr.insungjung.a0414_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+/*
+ID: tjeit, PW: qwer1234 입력된 상태로
+    로그인버튼 누르면 베인 액티비티로 이동
+    이이디 비번 위의 값이 아니라면, 토스트로 '로그인에 실패하셨습니다. ID/PW
+    */
+
+    EditText userIdText;
+    EditText userPasswordText;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userIdText = findViewById(R.id.idText);
+        userPasswordText = findViewById(R.id.passText);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userId = userIdText.getText().toString();
+                String userPw = userPasswordText.getText().toString();
+
+//                맞으면 메인화면으로 이동
+//                아니면 토스트 메세지
+
+                if (userId.equals("tjeit") && userPw.equals("qwer1234")) {
+//                  메인 액티비티로 이동
+
+                } else {
+//                    조건에 맞지 않다면
+                    Toast.makeText(LoginActivity.this, "로그인에 실패했습니다.\n ID/PW 를 확인해주세요!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
