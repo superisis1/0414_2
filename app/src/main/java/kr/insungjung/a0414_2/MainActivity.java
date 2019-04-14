b31a0483df08ee0b2050f76427d2b9112a35c217
Insung Jung <superisis@gmail.com>
2019-04-14 15:00:22 +0900

    String.xml 및 Manifest

--- app/src/main/java/kr/insungjung/a0414_2/MainActivity.java
+++ app/src/main/java/kr/insungjung/a0414_2/MainActivity.java
@@ -2,12 +2,39 @@ package kr.insungjung.a0414_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText searchText;
+    Button searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        searchText = findViewById(R.id.searchText);
+        searchBtn = findViewById(R.id.searchBtn);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String searchTxt = searchText.getText().toString();
+
+                String searchContent= String.format("%s를 검색합니다.",searchTxt);
+
+                Toast.makeText(MainActivity.this, searchContent, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
